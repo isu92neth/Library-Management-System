d2581ef2953ab750f9cfa042fedde5fcc2512287
isu92neth <123csissa@gmail.com>
2021-09-07 19:56:16 +0530

    Test style-components

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,8 +1,23 @@
+import styled from "styled-components/";
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <h1 style={{ textAlign: "center" }}>Hello Users!</h1>
-    </div>
+    <Wrapper>
+      <Title>
+        <h1 style={{ textAlign: "center" }}>Hello Users!</h1>
+      </Title>
+    </Wrapper>
   );
 }
 
