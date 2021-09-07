3bb7f1a3b1093c8e96fa75ad29e32efd5a7fd95d
isu92neth <123csissa@gmail.com>
2021-09-07 20:11:06 +0530

    Add main layout for the app

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,23 +1,12 @@
-import styled from "styled-components/";
-
-const Title = styled.h1`
-  font-size: 3em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+import { Header, Main, Footer } from "./components/Layout";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>
-        <h1 style={{ textAlign: "center" }}>Hello Users!</h1>
-      </Title>
-    </Wrapper>
+    <>
+      <Header>This is the Header</Header>
+      <Main>This is the Main</Main>
+      <Footer>This is the Footer</Footer>
+    </>
   );
 }
 
