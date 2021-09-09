36dbd1b2c1d51e3681e16008641eaf78993adcd0
isu92neth <123csissa@gmail.com>
2021-09-09 14:26:00 +0530

    Add theme provider

--- lms/src/App.js
+++ lms/src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
 import { Header, Main, Footer } from "./components/Layout";
 import { NavBar, NavItem, NavLink } from "./components/Navbar";
 function App() {
+  const theme = {
+    primary: {
+      main: "#29b6f6",
+      light: "#73e8ff",
+      dark: "#0086c3",
+      textColor: "#000",
+    },
+    secondary: {
+      main: "#fff",
+    },
+  };
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header>
         <NavBar>
           <NavItem href="#">
@@ -15,7 +29,7 @@ function App() {
       </Header>
       <Main>This is the Main</Main>
       <Footer>This is the Footer</Footer>
-    </>
+    </ThemeProvider>
   );
 }
 
