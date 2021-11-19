0b00911e62346328a29e289888f133e73dd1240b
isu92neth <123csissa@gmail.com>
2021-11-19 14:35:06 +0530

    Implement database integration for books

--- Server/server.js
+++ Server/server.js
@@ -32,105 +32,72 @@ server.use(express.json());
 server.use(cors());
 
 /* *************** Books ***************** */
-let books = [
-  {
-    id: "1",
-    title: "Harry Potter",
-    author: "J. K. Rowling",
-    isAvailable: true,
-    burrowedMemberId: "",
-    burrowedDate: "",
-  },
-  {
-    id: "2",
-    title: "Charlie and the Chocolate Factory",
-    author: "Roald Dahl",
-    isAvailable: true,
-    burrowedMemberId: "",
-    burrowedDate: "",
-  },
-];
 
 // /book: view all books
-server.get("/book", (req, res) => {
+server.get("/book", async (req, res) => {
+  const books = await Book.find();
   res.send(books);
 });
 
 // /book:id : View a book
-server.get("/book/:id", (req, res) => {
+server.get("/book/:id", async (req, res) => {
   const id = req.params.id;
-  const book = books.find((book) => book.id === id);
+  const book = await Book.findById(id);
   res.send(book);
 });
 
 // /book : POST: Create book
 // title, author
-server.post("/book", (req, res) => {
+server.post("/book", async (req, res) => {
   const { title, author } = req.body;
-
-  const book = {
-    id: Math.random().toString(16).slice(2),
-    title,
-    author,
-    isAvailable: true,
-    burrowedMemberId: "",
-    burrowedDate: "",
-  };
-  books.push(book);
-  res.send(book);
+  const book = new Book({ title, author });
+  const response = await book.save();
+  res.send(response);
 });
 
 // /book/:id : Edit a book
-server.put("/book/:id", (req, res) => {
+server.put("/book/:id", async (req, res) => {
   const id = req.params.id;
   const { title, author } = req.body;
-  const bookIndex = books.findIndex((book) => book.id === id);
-  books[bookIndex] = {
-    ...books[bookIndex],
+
+  const book = await Book.findByIdAndUpdate(id, {
     title,
     author,
-  };
-
-  res.send(books[bookIndex]);
+  });
+  res.send(book);
 });
 
 // /book/:id/burrow : Burrow a book
-server.put("/book/:id/burrow", (req, res) => {
+server.put("/book/:id/burrow", async (req, res) => {
   const id = req.params.id;
   const { burrowedMemberId, burrowedDate } = req.body;
 
-  const bookIndex = books.findIndex((book) => book.id === id);
-  books[bookIndex] = {
-    ...books[bookIndex],
+  const book = await Book.findByIdAndUpdate(id, {
     isAvailable: false,
     burrowedMemberId,
     burrowedDate,
-  };
-
-  res.send(books[bookIndex]);
+  });
+  res.send(book);
 });
 
 // /book/:id/return : Return a book
-server.put("/book/:id/return", (req, res) => {
+server.put("/book/:id/return", async (req, res) => {
   const id = req.params.id;
 
-  const bookIndex = books.findIndex((book) => book.id === id);
-  books[bookIndex] = {
-    ...books[bookIndex],
+  const book = await Book.findByIdAndUpdate(id, {
     isAvailable: true,
     burrowedMemberId: "",
     burrowedDate: "",
-  };
-
-  res.send(books[bookIndex]);
+  });
+  res.send(book);
 });
 
 // /book/:id : Delete a book
-server.delete("/book/:id", (req, res) => {
+server.delete("/book/:id", async (req, res) => {
   const id = req.params.id;
 
-  books = books.filter((book) => book.id !== id);
-  res.send(id);
+  const book = await Book.findByIdAndDelete(id);
+  res.send(book);
 });
 
 /* *************** Members ***************** */
